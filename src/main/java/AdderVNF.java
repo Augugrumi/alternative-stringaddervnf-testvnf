8958bf06949dd9ddd9c59cb4066e745fa1643a85
mzanella <zanna0150@gmail.com>
2018-08-20 12:14:27 +0200

    Fix adder vnf

--- src/main/java/AdderVNF.java
+++ src/main/java/AdderVNF.java
@@ -1,5 +1,8 @@
 import vfn.AbsBaseVNF;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class AdderVNF extends AbsBaseVNF {
 
     private String toAdd;
@@ -10,12 +13,21 @@ public class AdderVNF extends AbsBaseVNF {
     }
 
     public String functionality(String message) {
-        return message.replaceAll("\r\n\r\n", "\r\n") + toAdd;
+        Pattern pattern = Pattern.compile("Content-Length: \\d+");
+        Matcher matcher = pattern.matcher(message);
+        String match = "";
+        while (matcher.find()) {
+            match = matcher.group();
+        }
+        int length = Integer.parseInt(match.replaceAll("\\D+", "")) + toAdd.length();
+        message = message.replaceAll("\r\n\r\n", "\r\n") + toAdd;
+        message = message.replaceAll("Content-Length: \\d+", "Content-Length: " + length);
+        return message;
     }
 
     public static void main(String[] args) {
         System.out.println("string to add: " + args[0]);
         System.out.println("port:          " + args[1]);
-        new vfn.testvnfs.AddStringVNF(args[0], Integer.parseInt(args[1])).execute();
+        new AdderVNF(args[0], Integer.parseInt(args[1])).execute();
     }
 }
